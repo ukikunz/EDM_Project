8e136496c730a5d4e999adbc08620b9cfc5c22e9
eeysk8 <eeysk8@WEEP052677.ad.nottingham.ac.uk>
2017-03-21 11:20:52 +0000

    add metronome

---
+++ metronome.h
@@ -0,0 +1,21 @@
+
+#ifndef METRONOME_H
+#define	mETRONOME_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+void play_metronome(void);
+int SelectTimeSignature(void);
+int SelectSpeed(void);
+    
+    
+    
+    
+#ifdef	__cplusplus
+}
+#endif 
+
+#endif	/* METRONOME_H */
+
