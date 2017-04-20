902e486a554c4ecbd8ff593aa9913a343354d800
Saran Kritsanaviparkporn <eeysk8@nottingham.ac.uk>
2017-04-20 10:41:55 +0700

    added tuner.h

---
+++ tuner.h
@@ -0,0 +1,17 @@
+
+#ifndef TUNER_H
+#define TUNER_H
+
+
+#ifdef	__cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+    
+
+#ifdef	__cplusplus
+}
+#endif /* __cplusplus */
+
+#endif	/* TUNER_H */
+
